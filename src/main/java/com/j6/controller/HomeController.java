58caa101116be791be5c5b6bb365d6425711da3a
Test FPT Poly <test_fpt_poly@fpt.edu.vn>
2021-12-08 00:13:17 +0700

    suacong

--- src/main/java/com/j6/controller/HomeController.java
+++ src/main/java/com/j6/controller/HomeController.java
@@ -2,12 +2,11 @@ package com.j6.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String home(Model model) {
 		model.addAttribute("trangchu", true);
 		return "redirect:/home";
