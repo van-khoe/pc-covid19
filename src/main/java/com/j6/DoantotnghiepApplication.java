5300818d94ac1506e89b83181e655dad04a78b58
van-khoe <maihoc348@gmail.com>
2021-12-27 15:28:52 +0700

    add bean

--- src/main/java/com/j6/DoantotnghiepApplication.java
+++ src/main/java/com/j6/DoantotnghiepApplication.java
@@ -2,6 +2,9 @@ package com.j6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class DoantotnghiepApplication {
@@ -10,4 +13,16 @@ public class DoantotnghiepApplication {
 		SpringApplication.run(DoantotnghiepApplication.class, args);
 	}
 
+	@Bean
+	public ClassLoaderTemplateResolver secondaryTemplateResolver() {
+		ClassLoaderTemplateResolver secondaryTemplateResolver = new ClassLoaderTemplateResolver();
+		secondaryTemplateResolver.setPrefix("templates/");
+		secondaryTemplateResolver.setSuffix(".html");
+		secondaryTemplateResolver.setTemplateMode(TemplateMode.HTML);
+		secondaryTemplateResolver.setCharacterEncoding("UTF-8");
+		secondaryTemplateResolver.setOrder(1);
+		secondaryTemplateResolver.setCheckExistence(true);
+
+		return secondaryTemplateResolver;
+	}
 }
